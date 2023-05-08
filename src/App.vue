ad50402db6b6d126f4c85e5e1c7934b1232028d2
Nancy Lee <nramirez289@gmail.com>
2023-05-08 16:48:31 -0700

    initial commit for Jobs website

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <p>{{ jobs }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, toRefs, ref } from 'vue';
+import Job from './types/Jobs'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  components: {},
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
+
+    // state.name = 'Sean'
+
+    // return { ...toRefs(state) }
+
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'legal assistant', location: 'space', salary: 20, id: '2' },
+      { title: 'flute player', location: 'mom house', salary: 5, id: '3' },
+      { title: 'prison guard', location: 'the lost woods', salary: 10, id: '4' }
+    ])
+
+    return {jobs}
+    }
+  });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
