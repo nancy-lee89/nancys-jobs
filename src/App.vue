78391d0cdfbfd01bcff17c7801c47cf318ecc0fe
Nancy Lee <nramirez289@gmail.com>
2023-05-08 20:11:20 -0700

    modified app to use components and types for handle click events

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,62 @@
 <template>
   <div class="app">
-    <p>{{ jobs }}</p>
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <!-- JobList is the component, jobs is the prop that was defined in JobList.vue as 
+    a required PropType.'jobs' comes from the Job interface from the Job.ts file
+    the : are placed before each prop  -->
+    <JobList :jobs="jobs" :order="order"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, toRefs, ref } from 'vue';
-import Job from './types/Jobs'
+import JobList from './components/JobList.vue';
+import Job from './types/Job';
+import OrderTerm from './types/OrderTerm'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: { JobList },
   setup() {
-    // const state = reactive({
-    //   name: 'Link',
-    //   age: 25 as string | number
-    // })
-
-    // state.name = 'Sean'
-
-    // return { ...toRefs(state) }
-
     const jobs = ref<Job[]>([
       { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
       { title: 'legal assistant', location: 'space', salary: 20, id: '2' },
       { title: 'flute player', location: 'mom house', salary: 5, id: '3' },
       { title: 'prison guard', location: 'the lost woods', salary: 10, id: '4' }
     ])
+    const order = ref<OrderTerm>('title')
 
-    return {jobs}
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
     }
   });
 </script>
 
 <style>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 
 </style>
---
+++ src/types/OrderTerm.ts
@@ -0,0 +1,5 @@
+// this is saying that we can only use one of these three types
+
+type OrderTerm = 'location' | 'title' | 'salary'
+
+export default OrderTerm
